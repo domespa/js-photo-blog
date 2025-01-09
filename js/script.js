cf80a7cfd1801448b7c5f7ef5c68b8731eb3fb49
domespa <dumiii1988@gmail.com>
2025-01-09 15:28:48 +0100

    feat: add effects hover

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 //DOM ELM
-const cardContainerElm = document.getElementById("card-container")
+const cardContainerElm = document.getElementById("card-container");
+const cardImgElm = document.querySelectorAll(".card-img");
 
 axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
   .then((res) => {
@@ -9,7 +10,7 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
         <div class="col-sm-6 col-md-4 mb-5">
                     <div class="card p-3 position-relative">
                         <img src="../img/pin.svg" alt="pin" class="position-absolute top-0 start-50 translate-middle">
-                        <img src="${photo.url}" class="card-img" alt="Card image">
+                        <img id="modal" src="${photo.url}" class="card-img" alt="Card image">
                         <div class="card-body">
                             <p class="card-text">${photo.title}</p>
                         </div>
@@ -17,4 +18,4 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
                 </div>
       `;
     });
-  })
+  });
