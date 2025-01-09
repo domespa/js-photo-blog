9face381fd1158f37ee284cea44993455597efeb
domespa <dumiii1988@gmail.com>
2025-01-09 13:23:07 +0100

    feat: add mobile and tablet version

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
     const photos = res.data;
     photos.forEach((photo) => {
     cardContainerElm.innerHTML += `
-        <div class="col-md-4 mb-5">
+        <div class="col-sm-6 col-md-4 mb-5">
                     <div class="card p-3 position-relative">
                         <img src="../img/pin.svg" alt="pin" class="position-absolute top-0 start-50 translate-middle">
                         <img src="${photo.url}" class="card-img" alt="Card image">
