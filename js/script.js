3a6f5ce279ad4dd746f031ab3c0544e9a490ff7a
domespa <dumiii1988@gmail.com>
2025-01-13 12:59:33 +0100

    feat: add overlay

--- js/script.js
+++ js/script.js
@@ -1,21 +1,38 @@
-//DOM ELM
+// DOM ELM
 const cardContainerElm = document.getElementById("card-container");
-const cardImgElm = document.querySelectorAll(".card-img");
+const overlayElm = document.getElementById("overlay");
+const imgOvElm = document.getElementById("img-ov");
+const btnCloseElm = document.getElementById("btn-close");
 
-axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
+axios.get("https://picsum.photos/v2/list?page=1&limit=6")
   .then((res) => {
     const photos = res.data;
     photos.forEach((photo) => {
-    cardContainerElm.innerHTML += `
+      cardContainerElm.innerHTML += `
         <div class="col-sm-6 col-md-4 mb-5">
-                    <div class="card p-3 position-relative">
-                        <img src="../img/pin.svg" alt="pin" class="position-absolute top-0 start-50 translate-middle">
-                        <img id="modal" src="${photo.url}" class="card-img" alt="Card image">
-                        <div class="card-body">
-                            <p class="card-text">${photo.title}</p>
-                        </div>
-                    </div>
-                </div>
+          <div class="card p-3 position-relative">
+            <img src="../img/pin.svg" alt="pin" class="position-absolute top-0 start-50 translate-middle">
+            <img src="${photo.download_url}" class="card-img" alt="Card image">
+            <div class="card-body">
+              <p class="card-text">${photo.author}</p>
+            </div>
+          </div>
+        </div>
       `;
     });
+
+    const photoImgs = document.querySelectorAll(".card-img");
+
+    // CLICK ON IMG
+    photoImgs.forEach((photoImg) => {
+      photoImg.addEventListener("click", () => {
+        imgOvElm.src = photoImg.src; 
+        overlayElm.classList.remove("hidden"); 
+      });
+    });
   });
+
+// CLICK ON BTN
+btnCloseElm.addEventListener("click", () => {
+  overlayElm.classList.add("hidden");
+});
